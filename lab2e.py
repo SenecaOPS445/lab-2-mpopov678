29abcd9440d0c0b6f1f428ed634a47d5c03f9d0a
Michael Popov <mpopov@myseneca.ca>
2025-01-26 02:02:24 -0500

    lab2

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Michael Popov
+#Author ID: 107886202
+#Date Created: 2025/01/26
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
---
+++ lab2g.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#Author: Michael Popov
+#Author ID: 107886202
+#Date Created: 2025/01/26
+
+import sys
+
+if len(sys.argv) != 2:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
